ece926fcc45d7df38042f6c437fa159b940f78ba
Brandon Blankenstein <brandonblankenstein@gmail.com>
2025-10-23 23:26:40 -0400

    [api] move prisma to api
    enhance health check with basic crud operations
    
    add a test to the health module
    
    update prisma schema to handle GH actions

--- apps/api/src/modules/health/health.service.ts
+++ apps/api/src/modules/health/health.service.ts
@@ -1,12 +1,64 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma, Todo } from 'generated/prisma';
 
 @Injectable()
 export class HealthService {
+  constructor(private prisma: PrismaService) {}
+
   getHealth(): { ok: boolean; timestamp: string; message: string } {
     return {
       ok: true,
       timestamp: new Date().toISOString(),
       message: 'Health Check OK',
     };
+  }
+
+  async todo(
+    todoWhereUniqueInput: Prisma.TodoWhereUniqueInput,
+  ): Promise<Todo | null> {
+    return this.prisma.todo.findUnique({
+      where: todoWhereUniqueInput,
+    });
+  }
+
+  async todos(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.TodoWhereUniqueInput;
+    where?: Prisma.TodoWhereInput;
+    orderBy?: Prisma.TodoOrderByWithRelationInput;
+  }): Promise<Todo[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.todo.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async createTodo(data: Prisma.TodoCreateInput): Promise<Todo> {
+    return this.prisma.todo.create({
+      data,
+    });
+  }
+
+  async updateTodo(params: {
+    where: Prisma.TodoWhereUniqueInput;
+    data: Prisma.TodoUpdateInput;
+  }): Promise<Todo> {
+    const { where, data } = params;
+    return this.prisma.todo.update({
+      data,
+      where,
+    });
+  }
+
+  async deleteTodo(where: Prisma.TodoWhereUniqueInput): Promise<Todo> {
+    return this.prisma.todo.delete({
+      where,
+    });
   }
 }
