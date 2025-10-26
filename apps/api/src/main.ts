1f81e2d815cc573adb424ca5eea7fda431849653
Brandon Blankenstein <brandonblankenstein@gmail.com>
2025-10-26 16:43:12 -0400

    api: remove 'v1' endpoint prefix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// trigger docker build
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('v1');
   app.enableCors({
     origin: process.env.CORS_ORIGIN,
   });
