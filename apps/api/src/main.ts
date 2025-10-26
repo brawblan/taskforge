00329cf047397a53aa7abea0250abc7712de3608
Brandon Blankenstein <brandonblankenstein@gmail.com>
2025-10-26 16:59:11 -0400

    docker: add render deploy hook

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+// trigger docker build
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
