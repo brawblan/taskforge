575d123a1a9eea93fcbadd4ee8300619681ae52f
Brandon Blankenstein <brandonblankenstein@gmail.com>
2025-10-26 15:23:23 -0400

    update docker tag, trigger workflow

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// Make a change to trigger docker ci
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('v1');
