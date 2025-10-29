a585277edec1dbb5aa803655d81cd65b8391005b
Brandon Blankenstein <brandonblankenstein@gmail.com>
2025-10-28 22:39:41 -0400

    add projects module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,10 +2,17 @@ import { ConfigModule } from '@nestjs/config';
 import { Global, Module } from '@nestjs/common';
 import { HealthModule } from '@/modules/health/health.module';
 import { PrismaModule } from '@/modules/prisma/prisma.module';
+import { ProjectsModule } from './modules/projects/projects.module';
 import { UsersModule } from '@/modules/users/users.module';
 
 @Global()
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, HealthModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    HealthModule,
+    PrismaModule,
+    ProjectsModule,
+  ],
 })
 export class AppModule {}
---
+++ apps/api/src/modules/projects/projects.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { UpdateProjectDto } from './dto/update-project.dto';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class ProjectsService {
+  constructor(private prisma: PrismaService) {}
+
+  create(data: CreateProjectDto) {
+    return this.prisma.project.create({ data });
+  }
+
+  findAll(ownerId?: string) {
+    if (ownerId) {
+      return this.prisma.project.findMany({ where: { ownerId } });
+    }
+    return this.prisma.project.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prisma.project.findUnique({ where: { id } });
+  }
+
+  update(id: string, data: UpdateProjectDto) {
+    return this.prisma.project.update({
+      where: { id },
+      data,
+    });
+  }
+
+  remove(id: string) {
+    return this.prisma.project.delete({ where: { id } });
+  }
+}
